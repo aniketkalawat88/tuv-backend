2f89e45dc13c1a3a556f4e857a889dd179961d10
Aniket <aniketkalawat88@gmail.com>
2025-01-31 12:29:34 +0530

    cors

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://www.truevalueventures.in",  // Allow only your frontend domain
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,  // Allow cookies and authentication headers if needed
+  })
+);
 
 // Routes
 app.use("/api/blogs", blogRoutes);
